717b3bce30cc624367d2df3a16ae461029ed1df2
Sergio05-dot <sergiohcarvalho05@gmail.com>
2023-04-26 17:03:57 +0200

    interfaz para darle al JSON

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
+// Modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ItemComponent } from './pages/item/item.component';
 import { TerminosComponent } from './pages/terminos/terminos.component';
-import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -20,8 +24,9 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule, 
+    HttpClientModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
+  info: InfoPagina = {}; // Si no tenim la interfase InfoPagina seria de tipus any
+  cargada = false; // No cal dir boolean
+  constructor(private http: HttpClient) {
+    // console.log('Servicio de infoPagina listo');
+    // Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe((resp: any) => {
 
-  constructor() {
-    console.log("Servicio de infoPagina listo")
+        this.cargada = true;
+        this.info = resp; // provar resp. I veurem les propietats JSON
+        console.log(resp);
+      });
   }
 }
